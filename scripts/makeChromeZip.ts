feaf73357ab6c75caff632c084f69ef247b03727
Benjamin Van Renterghem <benjamin.v.renterghem@gmail.com>
2019-04-28 22:12:13 +0200

    Change zip script to make version-specific file names

--- scripts/makeChromeZip.ts
+++ scripts/makeChromeZip.ts
@@ -2,7 +2,21 @@ import fs from "fs";
 import path from "path";
 import archiver from "archiver";
 
-const zipPath = path.join(__dirname, "../dist/extension.zip");
+const extensionManifestPath = path.join(__dirname, "../build/manifest.json");
+if (!fs.existsSync(extensionManifestPath)) {
+    throw "No manifest.json found in the build directory.";
+}
+let extensionVersion: string;
+try {
+    extensionVersion = JSON.parse(fs.readFileSync(extensionManifestPath, { encoding: "utf-8" })).version;
+} catch {
+    throw "Could not get the version from the manifest.json in the build directory.";
+}
+if (extensionVersion === undefined || extensionVersion === null || extensionVersion === "") {
+    throw "Version from the manifest.json in the build directory should not be empty.";
+}
+
+const zipPath = path.join(__dirname, `../dist/SharepointDirectInvoke_v${extensionVersion}.zip`);
 const zipDirPath = path.dirname(zipPath);
 
 if (!fs.existsSync(zipDirPath)) {
